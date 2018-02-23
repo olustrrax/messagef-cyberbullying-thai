0a1f39902b9950ef8d11c2666066159e4506e6a2
olustrrax <olutrrax@gmail.com>
2018-02-23 17:16:16 +0700

    final 90 perscent

--- comparefeature.js
+++ comparefeature.js
@@ -1,10 +1,7 @@
 var fs = require('fs');
 var _ = require('lodash');
-var colnames = fs.readFileSync('./colnames1244.txt', 'utf8').split('\n');
-var colnames_fake = fs.readFileSync('./colnames1244_fake.txt', 'utf8').split('\n');
-var colname_before_fs = fs.readFileSync('./colname.txt', 'utf8').split('\n');
-var sizeFullFeature = _.size(colname_before_fs);
-var fullFeature = new Object
+var colnames = fs.readFileSync('./col90cor.txt', 'utf8').split('\r\n');
+var colnames_fake = fs.readFileSync('./col90cor_fake.txt', 'utf8').split('\r\n');
 var feature = new Object
 
 
@@ -13,7 +10,7 @@ module.exports = {
         sizeCol = _.size(colnames);
     
         for(var i=0; i<sizeCol; i++){
-            feature[colnames[i].replace('\r','')] = colnames_fake[i].replace('\r','');
+            feature[colnames[i]] = colnames_fake[i].replace(/[^0-9a-z]/gi, '');
         }
         return feature;
     }
@@ -23,31 +20,11 @@ module.exports = {
 function pair_feature(){
     sizeCol = _.size(colnames);
     
-    for(var i=0; i<sizeCol; i++){
-        feature[colnames[i].replace('\r','')] = colnames_fake[i].replace('\r','');
+    for(var i = 0;i<sizeCol;i++){
+        
+        feature[colnames[i]] = colnames_fake[i].replace(/[^0-9a-z]/gi, '');
+        console.log(feature[colnames[i]]+':'+colnames_fake[i])
     }
 }
+// pair_feature()
 
-// function feature_fakereal(){
-    
-//     var features = new Object
-//     sizeCol = _.size(colnames);
-        
-//     for(var i=0;i< sizeFullFeature; i++){
-//         for(var j=0; j<sizeCol;j++){
-//             if(_.keys(colnames)[j] == _.keys(fullFeature)[i]){
-//                 features[_.keys(colnames)[j]] = _.values(fullFeature);
-//             }
-//         }
-//     }
-//     console.log(features)
-// }
-
-
-
-// function createFullFeature(){
-//     for (var i=1; i<= sizeFullFeature; i++){
-//         fullFeature[colname_before_fs[i-1].replace('\r','')] = "a"+i;
-//     }
-    
-// }
